d3b23badff8b0e33f46d953c30539229f083fb84
Anthony <Anthony@DESKTOP-88CKNHU>
2018-03-28 18:46:26 +1300

    .

--- Configurator/App.xaml.cs
+++ Configurator/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Serilog;
 using System.Windows;
 
 namespace Configurator
@@ -13,5 +8,12 @@ namespace Configurator
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                    .WriteTo.File("Configurator.log", restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Verbose)
+                    .CreateLogger();
+        }
     }
 }
